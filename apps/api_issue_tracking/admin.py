1f7c5432b91647a4f9d0721d2a41a4130c893417
Valkea <github-sub@shedge.com>
2021-02-11 15:59:59 +0100

    refactor: register api_issue_tracking & users models in admin

--- apps/api_issue_tracking/admin.py
+++ apps/api_issue_tracking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, Issue, Comment, Contributor
+
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Comment)
+admin.site.register(Contributor)
