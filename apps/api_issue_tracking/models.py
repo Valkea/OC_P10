0acbc17c25a302de95d04d408daf9e094bad31bb
Valkea <github-sub@shedge.com>
2021-02-11 14:51:42 +0100

    feat: add the initial issue tracking models >> 'Issues', 'Projects', 'Comments'

--- apps/api_issue_tracking/models.py
+++ apps/api_issue_tracking/models.py
@@ -1,3 +1,136 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
 
-# Create your models here.
+# from apps.users.models import User  # TODO define AUTH_USER_MODEL in settings.py
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username="deleted")[0]
+
+
+class Project(models.Model):
+    class ProjectType(models.TextChoices):
+        BACKEND = 0, "Back-end"
+        FRONTEND = 1, "Front-end"
+        IOS = 2, "iOS"
+        ANDROID = 3, "Android"
+
+    title = models.CharField("Title", max_length=128)
+
+    description = models.TextField("Description", max_length=8192)
+
+    type = models.PositiveSmallIntegerField(
+        "Type", choices=ProjectType.choices, default=ProjectType.BACKEND
+    )
+
+    # author_user = models.ForeignKey(
+    #         to=settings.AUTH_USER_MODEL,
+    #         on_delete=models.SET(get_sentinel_user),
+    #         )
+
+
+class Issue(models.Model):
+    class Tag(models.TextChoices):
+        BUG = 0, "Bug"
+        FEAT = 1, "Amélioration"
+        TASK = 2, "Tâche"
+
+    class Priority(models.TextChoices):
+        LOW = 0, "Faible"
+        MEDIUM = 1, "Moyenne"
+        HIGH = 2, "Élevée"
+
+    class Status(models.TextChoices):
+        TODOS = 0, "À faire"
+        OPENED = 1, "En cours"
+        CLOSED = 2, "Terminé"
+
+    title = models.CharField("Title", max_length=128)
+
+    description = models.TextField("Description", max_length=8192)
+
+    tag = models.PositiveSmallIntegerField(
+        "Balise", choices=Tag.choices, default=Tag.BUG
+    )
+
+    priority = models.PositiveSmallIntegerField(
+        "Priorité", choices=Priority.choices, default=Priority.LOW
+    )
+
+    project = models.ForeignKey(
+        to=Project,
+        on_delete=models.CASCADE,
+        related_name="project_issues",
+    )
+
+    status = models.PositiveSmallIntegerField(
+        "Statut", choices=Status.choices, default=Status.TODOS
+    )
+
+    author_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET(get_sentinel_user),
+        related_name="author",
+    )
+
+    assignee_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET(get_sentinel_user),
+        related_name="assignees",
+    )
+
+    created_time = models.DateTimeField("Date de création", auto_now_add=True)
+
+    contributors = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="contributors")
+
+
+class Comment(models.Model):
+
+    description = models.TextField("Description", max_length=8192)
+
+    author_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET(get_sentinel_user),
+        related_name="comment_author",
+    )
+
+    issue = models.ForeignKey(
+        to=Issue,
+        on_delete=models.CASCADE,
+        related_name="comment_issue",
+    )
+
+    created_time = models.DateTimeField("Date de création", auto_now_add=True)
+
+
+# class Contributor(models.Model):  # TODO pas sur du tout !
+#
+#     class Role(models.TextChoices):
+#         OWNER = 0, "Responsable"
+#         CONTRIBUTOR = 1, "Contributeur"
+#
+#     class Permission(models.TextChoices):
+#         NONE = 0, "Aucune permission"
+#         READONLY = 1, "Lecture uniquement"
+#         ALL = 3, "Lecture & Ecriture"
+#
+#     user = models.ForeignKey(
+#         to=settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,  # TODO
+#         related_name="contributor_user",
+#     )
+#
+#     project = models.ForeignKey(
+#         to=Project,
+#         on_delete=models.CASCADE,
+#         related_name="project_contributors",
+#     )
+#
+#     permission = models.PositiveSmallIntegerField(
+#         "Permission", max_length=1, choices=Permission.choices, default=Permission.ALL
+#     )
+#
+#     role = models.PositiveSmallIntegerField(
+#         "Rôle", max_length=1, choices=Role.choices, default=Role.OWNER
+#     )
